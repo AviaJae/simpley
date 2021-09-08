13c42cc7b8c286af5ec34bb3acb7a8b798982b78
charweeeee <you@example.comecho>
2021-09-08 19:57:05 +0100

    finished template gen

--- src/templateGen.js
+++ src/templateGen.js
@@ -1,20 +1,27 @@
 function templateGen(answers){
-    
+const path = require('path');
 const fs = require('fs');
-var fse = require("fs-extra");
+const chalk = require('chalk');
+const errorCLI = require('./errorCLI');
+
+fs.mkdir(`./${answers.Name}`, (err) => {
+    if (err) {
+        throw err;
+    }
+});
 
 
-const Name = answers.Name 
-const Lang = answers.Language
-const Type = answers.Type
-const Token = answers.Token
+var copy = require('recursive-copy');
+
+copy(`./Simpley/temp/${answers.Language}/${answers.Type}`, `./${answers.Name}`, function(error, results) {
+	if (error) {
+		errorCLI("Unable To Create Files!")
+	} else {
+		console.log((chalk.bold.blue("Simpley:  ")) + (chalk.bold.bgGreenBright("SUCCESS") + ` Made ${results.length - 1} file(s)`));
+	}
+});
 
 
 
-
-
-
-
- 
 }
 module.exports = templateGen;
